6c02de7fc5d7adf166c7d1fe383b543c4ec2886b
0xhank <zeroxhank@gmail.com>
2023-06-14 20:11:38 -0400

    feat: working tests

--- packages/contracts/src/ERC721TestScript.sol
+++ packages/contracts/src/ERC721TestScript.sol
@@ -3,8 +3,6 @@ pragma solidity >=0.8.0;
 
 import { IBaseWorld } from "@latticexyz/world/src/interfaces/IBaseWorld.sol";
 import { ERC721Registration } from "@latticexyz/world/src/modules/tokens/erc721/ERC721Registration.sol";
-
-import { console } from "forge-std/console.sol";
 import {TokenLocation} from "./codegen/tables/TokenLocation.sol";
 import { ERC721TestToken, namespace, tableName, systemName } from "./ERC721TestToken.sol";
 
@@ -12,14 +10,13 @@ library ERC721TestScript {
   function run(address worldAddress ) internal{
     IBaseWorld world = IBaseWorld(worldAddress);
 
-    console.log('installing');
     ERC721Registration.install(world, namespace, "Test", "TST");
 
     world.registerTable(namespace, tableName, TokenLocation.getSchema(), TokenLocation.getKeySchema());
 
     //register the rest of the namespace's functionality like normal
     ERC721TestToken testToken = new ERC721TestToken();
-    world.registerSystem(namespace, systemName, testToken, false);
+    world.registerSystem(namespace, systemName, testToken, true);
 
   }
 }
--- packages/contracts/test/ERC721Test.t.sol
+++ packages/contracts/test/ERC721Test.t.sol
@@ -3,17 +3,12 @@ pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
 import { MudV2Test } from "@latticexyz/std-contracts/src/test/MudV2Test.t.sol";
-import { ERC721_S, METADATA_T } from "@latticexyz/world/src/modules/tokens/common/constants.sol";
-import { MetadataTable } from "@latticexyz/world/src/modules/tokens/common/MetadataTable.sol";
 import { IWorld } from "../src/codegen/world/IWorld.sol";
 import {ERC721Proxy} from "@latticexyz/world/src/modules/tokens/erc721/ERC721Proxy.sol";
 import {ResourceSelector } from "@latticexyz/world/src/ResourceSelector.sol";
 import {LibERC721} from "@latticexyz/world/src/modules/tokens/erc721/LibERC721.sol";
-import {ERC721TestToken, namespace} from "../src/ERC721TestToken.sol";
-import { ResourceType } from "@latticexyz/world/src/modules/core/tables/ResourceType.sol";
-import { Resource } from "@latticexyz/world/src/Types.sol";
+import {ERC721TestToken, namespace, systemName} from "../src/ERC721TestToken.sol";
 
-// I took these tests from https://github.com/Atarpara/openzeppeline-erc20-foundry-test
 contract ERC721Test is MudV2Test {
   IWorld public world;
   address public alice = address(uint160(0x6345659));
@@ -30,7 +25,7 @@ contract ERC721Test is MudV2Test {
 
 
   function call(bytes memory args) public returns (bytes memory) {
-      return world.call(namespace, ERC721_S, args);
+      return world.call(namespace, systemName, args);
   }
 
   modifier prank(address prankster) {
@@ -53,45 +48,45 @@ contract ERC721Test is MudV2Test {
       assertEq("Test", _name);
    }
 
-    // function testSymbol() external {
-    //     assertEq("ERC", token.symbol());
-    // }
+    function testSymbol() external {
+        assertEq("TST", token.symbol());
+    }
 
-    // function testMint() prank(alice) public {
-    //     call(abi.encodeWithSelector(ERC721TestToken.mint.selector, tokenId));
-    //     assertEq(token.ownerOf(69), alice);
-    //     assertEq(token.totalSupply(), 1);
-    //     assertEq(token.balanceOf(alice), 1);
-    // }
+    function testMint() prank(alice) public {
+        call(abi.encodeWithSelector(ERC721TestToken.mint.selector, tokenId));
+        assertEq(token.ownerOf(69), alice);
+        assertEq(token.totalSupply(), 1);
+        assertEq(token.balanceOf(alice), 1);
+    }
 
-    // function testBurn() public {
-    //    testMint();
+    function testBurn() public {
+       testMint();
 
-    //     call(abi.encodeWithSelector(ERC721TestToken.burn.selector, tokenId));
-    //     vm.expectRevert("ERC721: invalid token ID");
-    //     token.ownerOf(tokenId); // Reverts
-    // }
+        call(abi.encodeWithSelector(ERC721TestToken.burn.selector, tokenId));
+        vm.expectRevert("ERC721: invalid token ID");
+        token.ownerOf(tokenId); // Reverts
+    }
 
-    // function testTransfer() public {
-    //   testMint();
-    //   vm.prank(alice); 
-    //   call(abi.encodeWithSelector(ERC721TestToken.transfer.selector, bob, tokenId));
-    //   assertEq(token.ownerOf(69), bob);
-    //   assertEq(token.totalSupply(), 1);
-    //   assertEq(token.balanceOf(bob), 1);
-    //   assertEq(token.balanceOf(alice), 0);
-    // }
+    function testTransfer() public {
+      testMint();
+      vm.prank(alice); 
+      call(abi.encodeWithSelector(ERC721TestToken.transfer.selector, bob, tokenId));
+      assertEq(token.ownerOf(69), bob);
+      assertEq(token.totalSupply(), 1);
+      assertEq(token.balanceOf(bob), 1);
+      assertEq(token.balanceOf(alice), 0);
+    }
 
-    // function testPlace() public {
-    //   testMint();
-    //   vm.startPrank(alice);
-    //   uint256 x = 49;
-    //   uint256 y = 50;
-    //   call(abi.encodeWithSelector(ERC721TestToken.place.selector, tokenId, x, y));
-    //   bytes memory rawLocation = call(abi.encodeWithSelector(ERC721TestToken.location.selector, tokenId));
-    //   (uint256 retX, uint256 retY) = abi.decode(rawLocation, (uint256, uint256));
-    //   assertEq(retX, x);
-    //   assertEq(retY, y);
-    // }
+    function testPlace() public {
+      testMint();
+      vm.startPrank(alice);
+      uint256 x = 49;
+      uint256 y = 50;
+      call(abi.encodeWithSelector(ERC721TestToken.place.selector, tokenId, x, y));
+      bytes memory rawLocation = call(abi.encodeWithSelector(ERC721TestToken.location.selector, tokenId));
+      (uint256 retX, uint256 retY) = abi.decode(rawLocation, (uint256, uint256));
+      assertEq(retX, x);
+      assertEq(retY, y);
+    }
 
 }
